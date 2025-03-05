b79a448c674aef87bda3a084144f2d63811c0c94
Christopher A. Williamson <home@chrisaw.com>
2025-03-05 20:05:01 +0000

    Bump up to ap4a

--- makefiles/09_vndk_test_sepolicy.mk
+++ makefiles/09_vndk_test_sepolicy.mk
@@ -3,6 +3,6 @@ vndk-test-sepolicy: build-container
 	$(CONTAINER_RUN) voltage-gsi-builder \
 		/bin/bash -e -c 'pushd /work/repo/src && \
 		. build/envsetup.sh && \
-		lunch treble_arm64_bvN-ap1a-userdebug && \
+		lunch treble_arm64_bvN-ap4a-userdebug && \
 		make vndk-test-sepolicy -j$(CPU_LIMIT) && \
 		popd'
