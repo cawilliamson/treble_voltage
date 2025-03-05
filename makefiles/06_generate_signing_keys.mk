fd5db2d61448a5bc6d1af2df2aca617ddc2492e3
Christopher A. Williamson <contact@chrisaw.io>
2025-03-05 23:30:53 +0000

    Simplify signing keys step

--- makefiles/06_generate_signing_keys.mk
+++ makefiles/06_generate_signing_keys.mk
@@ -2,9 +2,6 @@
 generate-signing-keys: build-container create-folders
 	$(CONTAINER_RUN) voltage-gsi-builder \
 		/bin/bash -e -c ' \
-			pushd /work/repo/src && \
-				. build/envsetup.sh && \
-				pushd vendor/voltage-priv/keys && \
-					./keys || true && \
-				popd && \
+			pushd /work/repo/src/pushd vendor/voltage-priv/keys && \
+				./keys || true && \
 			popd'
