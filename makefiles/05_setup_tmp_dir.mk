75316015318d86c3d9d9191344a47a17f550a54c
Christopher A. Williamson <home@chrisaw.com>
2025-03-05 23:30:53 +0000

    Simplify signing keys step

--- makefiles/05_setup_tmp_dir.mk
+++ makefiles/05_setup_tmp_dir.mk
@@ -1,14 +1,7 @@
 # Step 5: Setup tmp directory and stash gapps variants
 setup-tmp-dir: build-container create-folders
 	$(CONTAINER_RUN) voltage-gsi-builder \
-		/bin/bash -e -c 'mkdir -p /work/tmp/ && \
-			if [ -d "/work/repo/src/vendor/gapps" ]; then \
-				mv -v /work/repo/src/vendor/gapps /work/tmp/; \
-			else \
-				echo "vendor/gapps not found, skipping"; \
-			fi && \
-			if [ -d "/work/repo/src/vendor/partner_gms" ]; then \
-				mv -v /work/repo/src/vendor/partner_gms /work/tmp/; \
-			else \
-				echo "vendor/partner_gms not found, skipping"; \
-			fi'
+		/bin/bash -e -c ' \
+			mkdir -p /work/tmp/ && \
+			mv -v /work/repo/src/vendor/gapps /work/tmp/ && \
+			mv -v /work/repo/src/vendor/partner_gms /work/tmp/'
