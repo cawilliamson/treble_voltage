0f7991f41bd4c698b3719c9879bf91f9b029b637
Christopher A. Williamson <home@chrisaw.com>
2025-03-05 23:33:15 +0000

    Clean up signing keys code

--- makefiles/06_generate_signing_keys.mk
+++ makefiles/06_generate_signing_keys.mk
@@ -2,6 +2,6 @@
 generate-signing-keys: build-container create-folders
 	$(CONTAINER_RUN) voltage-gsi-builder \
 		/bin/bash -e -c ' \
-			pushd /work/repo/src/pushd vendor/voltage-priv/keys && \
+			pushd /work/repo/src/vendor/voltage-priv/keys && \
 				./keys || true && \
 			popd'
