298ad65c3b1235487f3d9415467cfa50786037c6
Christopher A. Williamson <home@chrisaw.com>
2025-03-06 21:08:46 +0000

    Remove vndklite support

--- makefiles/10_rename_images.mk
+++ makefiles/10_rename_images.mk
@@ -9,10 +9,4 @@ rename-images: build-container create-folders
 				mv -v system_vanilla_a64.img "VoltageOS"-vanilla-arm32_binder64-ab-"$${ROM_VERSION}"-"$${BUILD_DATE}"-UNOFFICIAL.img && \
 				mv -v system_microg_a64.img "VoltageOS"-microg-arm32_binder64-ab-"$${ROM_VERSION}"-"$${BUILD_DATE}"-UNOFFICIAL.img && \
 				mv -v system_gapps_a64.img "VoltageOS"-gapps-arm32_binder64-ab-"$${ROM_VERSION}"-"$${BUILD_DATE}"-UNOFFICIAL.img && \
-				mv -v s_vanilla_arm64_vndklite.img "VoltageOS"-vanilla-arm64-ab-vndklite-"$${ROM_VERSION}"-"$${BUILD_DATE}"-UNOFFICIAL.img && \
-				mv -v s_microg_arm64_vndklite.img "VoltageOS"-microg-arm64-ab-vndklite-"$${ROM_VERSION}"-"$${BUILD_DATE}"-UNOFFICIAL.img && \
-				mv -v s_gapps_arm64_vndklite.img "VoltageOS"-gapps-arm64-ab-vndklite-"$${ROM_VERSION}"-"$${BUILD_DATE}"-UNOFFICIAL.img && \
-				mv -v s_vanilla_a64_vndklite.img "VoltageOS"-vanilla-arm32_binder64-ab-vndklite-"$${ROM_VERSION}"-"$${BUILD_DATE}"-UNOFFICIAL.img && \
-				mv -v s_microg_a64_vndklite.img "VoltageOS"-microg-arm32_binder64-ab-vndklite-"$${ROM_VERSION}"-"$${BUILD_DATE}"-UNOFFICIAL.img && \
-				mv -v s_gapps_a64_vndklite.img "VoltageOS"-gapps-arm32_binder64-ab-vndklite-"$${ROM_VERSION}"-"$${BUILD_DATE}"-UNOFFICIAL.img && \
 			popd'
