df5153ae23f48fcb402f7af62da26e5fe6ce594e
Christopher A. Williamson <contact@chrisaw.io>
2025-05-01 21:21:01 +0100

    Remove hard-set boot animation size - allow TD patch to handle scaling

--- configs/voltage-microg.mk
+++ configs/voltage-microg.mk
@@ -3,9 +3,6 @@ $(call inherit-product, vendor/voltage/config/BoardConfigVoltage.mk)
 $(call inherit-product, device/voltage/sepolicy/common/sepolicy.mk)
 -include vendor/voltage/build/core/config.mk
 
-# Bootanimation (force 720p - 720x1280)
-TARGET_BOOT_ANIMATION_RES := 1280
-
 # Kernel
 TARGET_NO_KERNEL_IMAGE := true
 TARGET_NO_KERNEL_OVERRIDE := true
